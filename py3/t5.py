e9aade7560d83ed2a34f7169de26a6881694a193
lwq6783293 <573925242@qq.com>
2018-01-16 18:28:09 +0800

    上传条件判断语句练习

--- py3/t5.py
+++ py3/t5.py
@@ -7,4 +7,34 @@
 # @Software: PyCharm
 # TODO 条件判断练习
 
+# TODO if
+age = 20
+if age >= 18:
+    print('your age is',age)
+    print('adult')
+
+print('------------------------------------------------------------------------')
+
+# 也可以给if添加一个else语句，意思是，如果if判断是False，不要只想if的内容，去把else执行了
+age = 3
+if age >= 18:
+    print('your age is',age)
+    print('adult')
+else:
+    print('your age is',age)
+    print('teenager')
+
+print('------------------------------------------------------------------------')
+
+# 使用elif做更细致的判断
+age = 3
+if age >= 18:
+    print('adult')
+elif age >= 6:
+    print('teenager')
+else:
+    print('kid')
+
+print('------------------------------------------------------------------------')
+
 
